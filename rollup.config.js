95630a2f311cd0a9e8ad28e9177ca45470c5a9b1
luckyadam <weitaozsh@gmail.com>
2017-08-30 23:19:51 +0800

    feat:build to production

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import fs from 'fs'
+
+const babelConfig = JSON.parse(String(fs.readFileSync('.babelrc')))
+babelConfig.plugins.push('external-helpers')
+
+export default {
+  name: 'jsonp-retry',
+  input: 'index.js',
+  output: {
+    file: 'dist/jsonp-retry.dev.js',
+    format: 'umd',
+    sourcemap: true
+  },
+  plugins: [
+    resolve({
+      main: true,
+      jsnext: true
+    }),
+    commonjs(),
+    babel({
+      sourceMap: true,
+      babelrc: false,
+      presets: [
+        ['es2015', {
+          modules: false,
+          loose: true
+        }],
+        'stage-0'
+      ],
+      plugins: babelConfig.plugins,
+      exclude: 'node_modules/**'
+    })
+  ]
+}
