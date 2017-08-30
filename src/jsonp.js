8196f02816486c266a577fa00e8330d4ade8b97f
luckyadam <weitaozsh@gmail.com>
2017-08-30 19:48:39 +0800

    feat:timeout && retry

--- src/jsonp.js
+++ src/jsonp.js
@@ -11,20 +11,26 @@ const canUsePromise = (function () {
 
 const encodeC = encodeURIComponent
 const doc = document
+const head = doc.head || doc.getElementsByTagName('head')[0]
+
+const TIMEOUT_CONST = 2000
 
 const defaultConfig = {
-  timeout: 2000,
+  timeout: TIMEOUT_CONST,
   retryTimes: 2,
   backup: null,
   params: {},
   jsonp: 'callback',
-  jsonpCallback: null,
+  name: null,
   needStore: false,
   cache: true,
   storeSign: null,
   dataCheck: null,
   scriptCharset: 'UTF-8'
 }
+
+let timer
+let script
 
 function jsonp (url, opts, cb) {
   if (typeof url === 'function') {
@@ -44,26 +50,52 @@ function jsonp (url, opts, cb) {
   }
   opts = assign({}, defaultConfig, opts)
   url = url || opts.url
+  fetchData(url, opts, cb)
+}
+
+function fetchData (url, opts, cb) {
   if (!url || typeof url !== 'string') {
-    return cb(new Error('Param url is needed!'))
+    if (false === fallback(opts, cb)) {
+      return cb(new Error('Param url is needed!'))
+    }
+    return
   }
-  const backup = opts.backup
   const charset = opts.scriptCharset
-  const funcId = opts.jsonpCallback || `__jsonp${new Date().getTime()}`
+  const funcId = opts.name || `__jsonp${new Date().getTime()}`
   let params = serializeParams(opts.params)
   if (params) {
     params = `&${params}`
   }
   url += (~url.indexOf('?') ? '&' : '?') + `${opts.jsonp}=${encodeC(funcId)}${params}`
   url = url.replace('?&', '?')
+  const timeout = opts.timeout != null ? opts.timeout : TIMEOUT_CONST
+
+  // when timeout, will try to retry
+  timer = setTimeout(() => {
+    cleanup(funcId)
+    // no retryTimes left, go to backup
+    if (typeof opts.retryTimes === 'number' && opts.retryTimes > 0) {
+      opts.retryTimes--
+      return fetchData(url, opts, cb)
+    }
+    if (false === fallback(opts, cb)) {
+      return cb(new Error('Timeout and no data return'))
+    }
+  }, timeout)
+
   win[funcId] = function (data) {
+    cleanup(funcId)
     if (opts.dataCheck) {
       if (opts.dataCheck(data)) {
-        cb(null, data)
+        return cb(null, data)
       }
-      return
+      const backup = opts.backup
+      if (false === fallback(opts, cb)) {
+        cb(new Error('Data check error, and no fallback'))
+      }
+    } else {
+      cb(null, data)
     }
-    cb(null, data)
   }
   appendScriptTagToHead({
     url,
@@ -71,16 +103,36 @@ function jsonp (url, opts, cb) {
   })
 }
 
-function appendScriptTagToHead ({ url, charset }) {
-  const head = doc.head || doc.getElementsByTagName('head')[0]
-  const script = doc.createElement('script')
+function fallback (opts, cb) {
+  const backup = opts.backup
+  if (backup) {
+    if (Array.isArray(backup) && backup.length) {
+      return fetchData(backup.shift(), opts, cb)
+    }
+    delete opts.backup
+    return fetchData(backup, opts, cb)
+  }
+  return false
+}
+
+function cleanup (funcId) {
+  if (script.parentNode) {
+    script.parentNode.removeChild(script)
+  }
+  win[funcId] = noop
+  if (timer) {
+    clearTimeout(timer)
+  }
+}
+
+export function appendScriptTagToHead ({ url, charset }) {
+  script = doc.createElement('script')
   script.type = 'text/javascript'
   if (charset) {
     script.charset = charset
   }
   script.src = url
   head.appendChild(script)
-  return script
 }
 
 export default jsonp
--- test/jsonp.test.js
+++ test/jsonp.test.js
@@ -1,9 +1,14 @@
-import jsonp from '../src/jsonp'
+import jsonp from '../index'
 
 const TEST_URL = 'http://json.diao.li/getjson/59a5671ee6da184a056ce9ae'
+const BACKUP_URL1 = 'http://json.diao.li/getjson/59a66a9fe6da184a056ce9af'
+const BACKUP_URL2 = 'http://json.diao.li/getjson/59a67cd3e6da184a056ce9b0'
+const TIMEOUT_UTL = 'http://json.diao.li/getjson/59a685ade6da184a056ce9b1'
 
-describe('test jsonp', () => {
-  it('test jsonp called with no url param', () => {
+describe('test jsonp', function () {
+  this.timeout(2000000)
+
+  it('test jsonp called with no url && backup param', () => {
     jsonp(null, {}, function (err) {
       expect(err).to.exist
         .and.be.instanceof(Error)
@@ -33,14 +38,17 @@ describe('test jsonp', () => {
     })
   })
 
-  it('test basic jsonp support with callback', () => {
+  it('test basic jsonp support with callback', (done) => {
     jsonp(TEST_URL, function (err, data) {
       expect(err).to.not.exist
       expect(data).to.have.deep.nested.property('code', 0)
       expect(data).to.have.deep.nested.property('data.count', 10)
       expect(data).to.have.deep.nested.property('data.arr', [1, 2, 3])
+      done()
     })
+  })
 
+  it('test basic jsonp support with callback', (done) => {
     jsonp({
       url: TEST_URL
     }, function (err, data) {
@@ -48,6 +56,106 @@ describe('test jsonp', () => {
       expect(data).to.have.deep.nested.property('code', 0)
       expect(data).to.have.deep.nested.property('data.count', 10)
       expect(data).to.have.deep.nested.property('data.arr', [1, 2, 3])
+      done()
+    })
+  })
+
+  it('test custom jsonp callback name', (done) => {
+    jsonp(TEST_URL, {
+      name: 'customJsonpCallback'
+    }, function (err, data) {
+      expect(err).to.not.exist
+      expect(data).to.have.deep.nested.property('code', 0)
+      expect(data).to.have.deep.nested.property('data.count', 10)
+      expect(data).to.have.deep.nested.property('data.arr', [1, 2, 3])
+      done()
+    })
+  })
+
+  it('test jsonp use backup string if no url param', (done) => {
+    jsonp({
+      backup: BACKUP_URL2
+    }, function (err, data) {
+      expect(err).to.not.exist
+      expect(data).to.have.deep.nested.property('code', 1)
+      expect(data).to.have.deep.nested.property('name', 'backup2')
+      expect(data).to.have.deep.nested.property('data.count', 1)
+      expect(data).to.have.deep.nested.property('data.arr', [1])
+      done()
+    })
+  })
+
+  it('test jsonp will not use backup string if has url', (done) => {
+    jsonp(TEST_URL, {
+      backup: BACKUP_URL1
+    }, function (err, data) {
+      expect(err).to.not.exist
+      expect(data).to.have.deep.nested.property('code', 0)
+      expect(data).to.have.deep.nested.property('data.count', 10)
+      expect(data).to.have.deep.nested.property('data.arr', [1, 2, 3])
+      done()
+    })
+  })
+
+  it('test jsonp will use backup string if url data check fail', (done) => {
+    jsonp(TEST_URL, {
+      backup: BACKUP_URL1,
+      dataCheck (data) {
+        if (data.code === 0) {
+          return false
+        }
+        return true
+      }
+    }, function (err, data) {
+      expect(err).to.not.exist
+      expect(data).to.have.deep.nested.property('code', 1)
+      expect(data).to.have.deep.nested.property('data.count', 1)
+      expect(data).to.have.deep.nested.property('data.arr', [1])
+      done()
+    })
+  })
+
+  it('test jsonp will not use backup array if has url', (done) => {
+    jsonp(TEST_URL, {
+      backup: [BACKUP_URL1, BACKUP_URL2],
+      dataCheck (data) {
+        if (data.name !== 'backup2') {
+          return false
+        }
+        return true
+      }
+    }, function (err, data) {
+      expect(err).to.not.exist
+      expect(data).to.have.deep.nested.property('code', 1)
+      expect(data).to.have.deep.nested.property('name', 'backup2')
+      expect(data).to.have.deep.nested.property('data.count', 1)
+      expect(data).to.have.deep.nested.property('data.arr', [1])
+      done()
+    })
+  })
+
+  it('test jsonp timeout', (done) => {
+    jsonp(TIMEOUT_UTL, {
+      timeout: 1000
+    }, function (err, data) {
+      expect(err).to.exist
+        .and.be.instanceof(Error)
+        .and.have.property('message', 'Timeout and no data return')
+      done()
+    })
+  })
+
+  it('test jsonp will retry when timeout', (done) => {
+    jsonp(TIMEOUT_UTL, {
+      timeout: 1000,
+      retryTimes: 1,
+      backup: BACKUP_URL1
+    }, function (err, data) {
+      expect(err).to.not.exist
+        expect(data).to.have.deep.nested.property('code', 1)
+        expect(data).to.have.deep.nested.property('data.count', 1)
+        expect(data).to.have.deep.nested.property('data.arr', [1])
+        done()
     })
   })
 })
